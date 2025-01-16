2196fa4e80757ab51644db137a719b056b8061ba
IsaacToluwani <toluwani.isaac@gmail.com>
2025-01-16 15:36:56 +0100

    Add Product Data

---
+++ src/Assets/all_product.js
@@ -0,0 +1,213 @@
+import p1_img from './product_1.png';
+import p2_img from './product_2.png';
+import p3_img from './product_3.png';
+import p4_img from './product_4.png';
+import p5_img from './product_5.png';
+import p6_img from './product_6.png';
+import p7_img from './product_7.png';
+import p8_img from './product_8.png';
+import p9_img from './product_9.png';
+import p10_img from './product_10.png';
+import p11_img from './product_11.png';
+import p12_img from './product_12.png';
+import p13_img from './product_13.png';
+import p14_img from './product_14.png';
+import p15_img from './product_15.png';
+import p16_img from './product_16.png';
+import p17_img from './product_17.png';
+import p18_img from './product_18.png';
+import p19_img from './product_19.png';
+import p20_img from './product_20.png';
+import p21_img from './product_21.png';
+import p22_img from './product_22.png';
+import p23_img from './product_23.png';
+import p24_img from './product_24.png';
+
+const all_product = [
+  {
+    id: 1,
+    name: 'Best Wireless Over-Ear Headphones High-Fidelity Audio',
+    image: p1_img,
+    category: 'headphone',
+    new_price: 20000,
+    old_price: 25000,
+  },
+  {
+    id: 2,
+    name: 'Red and Black Gaming Headset PNG Immersive Stereo Sound',
+    image: p2_img,
+    category: 'headphone',
+    new_price: 21000,
+    old_price: 25000,
+  },
+  {
+    id: 3,
+    name: 'RGB Gaming Headset Immersive Sound, Neon Lights, Wired Headphones',
+    image: p3_img,
+    category: 'headphone',
+    new_price: 26000,
+    old_price: 27000,
+  },
+  {
+    id: 4,
+    name: 'RGB LED Gaming Headphones Immersive Audio & Neon Lights',
+    image: p4_img,
+    category: 'headphone',
+    new_price: 22000,
+    old_price: 24000,
+  },
+  {
+    id: 5,
+    name: 'Silver Over-Ear Headphones High-Quality Audiophile',
+    image: p5_img,
+    category: 'headphone',
+    new_price: 23000,
+    old_price: 24000,
+  },
+  {
+    id: 6,
+    name: 'Stylish Red & White Gaming Headphones  Immersive Audio Experience',
+    image: p6_img,
+    category: 'headphone',
+    new_price: 22500,
+    old_price: 24000,
+  },
+  {
+    id: 7,
+    name: 'Stylish Red & White Gaming Headphones Immersive Audio Experience',
+    image: p7_img,
+    category: 'headphone',
+    new_price: 19000,
+    old_price: 22000,
+  },
+  {
+    id: 8,
+    name: 'Wireless Over-Ear Headphones Burgundy & Silver Stereo Audio Headset',
+    image: p8_img,
+    category: 'headphone',
+    new_price: 21000,
+    old_price: 23000,
+  },
+  {
+    id: 9,
+    name: 'CMOTION Wireless Optical Computer Mouse',
+    image: p9_img,
+    category: 'headphone',
+    new_price: 22500,
+    old_price: 24000,
+  },
+  {
+    id: 10,
+    name: 'Ergonomic Wireless Computer Mouse',
+    image: p10_img,
+    category: 'mouse',
+    new_price: 23000,
+    old_price: 24500,
+  },
+  {
+    id: 11,
+    name: 'Ergonomic Wireless Gaming Mouse',
+    image: p11_img,
+    category: 'mouse',
+    new_price: 21000,
+    old_price: 23300,
+  },
+  {
+    id: 12,
+    name: 'Futuristic RGB Gaming Mouse',
+    image: p12_img,
+    category: 'mouse',
+    new_price: 22000,
+    old_price: 25000,
+  },
+  {
+    id: 13,
+    name: 'Grefaicieich Wireless Mouse',
+    image: p13_img,
+    category: 'mouse',
+    new_price: 21100,
+    old_price: 27000,
+  },
+  {
+    id: 14,
+    name: 'RGB LED Wireless Gaming Mouse',
+    image: p14_img,
+    category: 'mouse',
+    new_price: 20000,
+    old_price: 29000,
+  },
+  {
+    id: 15,
+    name: 'White Wireless Gaming Mouse with Integrated Keypad',
+    image: p15_img,
+    category: 'mouse',
+    new_price: 19000,
+    old_price: 20000,
+  },
+  {
+    id: 16,
+    name: 'Wireless Computer Mouse PNG Ergonomic Design, White',
+    image: p16_img,
+    category: 'mouse',
+    new_price: 26000,
+    old_price: 27000,
+  },
+  {
+    id: 17,
+    name: 'Boombotix Boombot Rex Portable Bluetooth Speaker',
+    image: p17_img,
+    category: 'radio',
+    new_price: 30000,
+    old_price: 32000,
+  },
+  {
+    id: 18,
+    name: 'Portable Bluetooth Party Speaker with LED Lights - Boombox',
+    image: p18_img,
+    category: 'radio',
+    new_price: 31000,
+    old_price: 32000,
+  }, {
+    id: 19,
+    name: 'Red and White Boombox Speaker System',
+    image: p19_img,
+    category: 'radio',
+    new_price: 30000,
+    old_price: 32500,
+  }, {
+    id: 20,
+    name: 'Retro Boombox Clock Radio with Alarm, Bluetooth Speaker',
+    image: p20_img,
+    category: 'radio',
+    new_price: 30300,
+    old_price: 32000,
+  }, {
+    id: 21,
+    name: 'Retro Boombox Radio Vintage Teal Bluetooth Speaker',
+    image: p21_img,
+    category: 'radio',
+    new_price: 31000,
+    old_price: 32000,
+  }, {
+    id: 22,
+    name: 'Retro Boombox with Clock Radio, Bluetooth Speaker',
+    image: p22_img,
+    category: 'radio',
+    new_price: 29000,
+    old_price: 32500,
+  }, {
+    id: 23,
+    name: 'Jdeanu Retro Boombox Radio with Clock',
+    image: p19_img,
+    category: 'radio',
+    new_price: 30000,
+    old_price: 32000,
+  }, {
+    id: 24,
+    name: 'Retro Red Boombox Speaker System',
+    image: p19_img,
+    category: 'radio',
+    new_price: 30000,
+    old_price: 32000,
+  },
+];
