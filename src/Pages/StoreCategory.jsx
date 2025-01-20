74e21da34d26c4c93bfe0f989064f33507505963
IsaacToluwani <toluwani.isaac@gmail.com>
2025-01-20 10:23:53 +0100

    Store Category Pages: Render products based on Categories

--- src/Pages/StoreCategory.jsx
+++ src/Pages/StoreCategory.jsx
@@ -1,27 +1,37 @@
 import React, { useContext } from 'react';
 import { StoreContext } from '../Context/StoreContext';
 import Item from '../Components/Item/Item';
+//import { useParams } from 'react-router-dom';
 //import all_product from '../Assets/all_product';
 
 function StoreCategory(props) {
   const { all_product } = useContext(StoreContext);
+
   if (!all_product) {
-    return <h1>product not found</h1>;
+    return <h1> product not found</h1>;
   }
 
   return (
     <div className='store-category'>
+     
+
       {all_product.map((product, i) => {
         if (props.category === product.category) {
-          return <Item name={product.name} image={product.image} />;
+          return (
+            <Item
+          id={product.id}
+          key={i}
+          image={product.image}
+          name={product.name}
+          new_price={product.new_price}
+          old_price={product.old_price}
+          category={product.category}
+        />
+          )
         }
       }
-    
+      )}
     </div>
-
-}    
-    
-
-
-
+  );
+}
 export default StoreCategory;
