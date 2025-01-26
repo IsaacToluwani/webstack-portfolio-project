530d23a2b98225a109f18929839d4664d5e9c2c5
IsaacToluwani <toluwani.isaac@gmail.com>
2025-01-26 21:39:11 +0100

    style store category

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 function Item(props) {
   return (
     <div className='item'>
-      <Link to={`/product/${props.id}`}>
+      <Link to={`/product/${props.id}` }>
         <img src={props.image} alt='' className='item-image' />
+        <h4>{props.name}</h4>
       </Link>
       
-        <h4>{props.name}</h4>
-      <div className=''>
+      <div className='pricing'>
         <p className='new-price'>${props.new_price} </p>
         <p className='old-price'>${props.old_price} </p>
       </div>
--- src/Pages/StoreCategory.jsx
+++ src/Pages/StoreCategory.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import './CSS/StoreCategory.css'
 import { StoreContext } from '../Context/StoreContext';
 import Item from '../Components/Item/Item';
 //import { useParams } from 'react-router-dom';
@@ -12,26 +13,29 @@ function StoreCategory(props) {
   }
 
   return (
-    <div className='store-category'>
-     
-
+    <div className="store-category">
+      <div className="store-banner">
+        <h1>{props.category }</h1>
+      </div>
+    <div className='store-category-product'>
       {all_product.map((product, i) => {
         if (props.category === product.category) {
           return (
             <Item
-          id={product.id}
-          key={i}
-          image={product.image}
-          name={product.name}
-          new_price={product.new_price}
-          old_price={product.old_price}
-          category={product.category}
-        />
-          )
-        }
-      }
-      )}
+              id={product.id}
+              key={i}
+              image={product.image}
+              name={product.name}
+              new_price={product.new_price}
+              old_price={product.old_price}
+              category={product.category}
+              />
+              
+            );
+          }
+        })}
     </div>
+        </div>
   );
 }
 export default StoreCategory;
