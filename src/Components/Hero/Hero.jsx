7bbb7c7c4a0508fee2bc5ff56e7d46ff96c6ae3f
IsaacToluwani <toluwani.isaac@gmail.com>
2025-01-26 20:56:46 +0100

    Style Trending Items

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import './Hero.css'
-import mouse from '../../Assets/product_5.png'
+import React from 'react';
+import './Hero.css';
+import mouse from '../../Assets/product_5.png';
 
 function Hero() {
   return (
     <div className='hero'>
-      <div className="hero-left">
-<h1>bestsdfsdf</h1>
+      
+      <div className='hero-left'>
+      <h4>Your One Stop Location</h4>
+        <h1>Get The Best Electronics</h1>
+        <button className='btn'>Visit Shop</button>
       </div>
-      <div className="hero-img">
-        <img src={ mouse} alt="" />
+      <div className='hero-img'>
+        <img src={mouse} alt='' />
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
